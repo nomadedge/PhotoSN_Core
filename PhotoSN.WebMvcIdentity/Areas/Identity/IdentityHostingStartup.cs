8ef61e67953675b233c5c82e043d510feddc528c
nomadedge <nomad.edge@gmail.com>
2020-01-16 14:05:01 +0300

    scaffold identity markup and logic for customization

---
+++ PhotoSN.WebMvcIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhotoSN.Data.DbContexts;
+using PhotoSN.Data.Entities;
+
+[assembly: HostingStartup(typeof(PhotoSN.WebMvcIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace PhotoSN.WebMvcIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
